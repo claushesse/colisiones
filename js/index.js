0dd30c7e50f6ce2688ca32a32272c422e54cfce0
Claus Hesse <hesseclaus@gmail.com>
2019-09-20 16:54:54 -0300

    Changes
    Agrego ADSR
    Cambio interfaz menu.

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var sliderRelease;
 var smoothedTime = 0;
 var actTime;
 var menu;
-var boton;
+var boton; 
 var canvas;
 var xcanvas;
 var ycanvas;
@@ -31,6 +31,16 @@ var square;
 var triangle;
 var sawtooth;
 var context = new AudioContext();
+var divDiameterSlider;
+var divCheckbox;
+var divSelectWaveform;
+var divTimeSlider;
+var divFeedBackSlider;
+var divReverbSlider;
+var divattackSlider;
+var divdecaySlider;
+var divsustainSlider;
+var divreleaseSlider;
 
 function setup(){
 
@@ -38,7 +48,18 @@ function setup(){
 	canvas.parent("myContainer");
 	centerCanvas();
 	textFont("Helvetica");
-	
+
+	menu = document.getElementById("mySidenav").clientWidth;
+	divDiameterSlider = document.getElementById("diameterSlider");
+	divCheckbox = document.getElementById("checkbox");
+	divSelectWaveform = document.getElementById("selectWaveform");;
+	divTimeSlider = document.getElementById("timeSlider");;
+	divFeedBackSlider = document.getElementById("feedbackSlider");;
+	divReverbSlider = document.getElementById("reverbSlider");;
+	divAttackSlider = document.getElementById("attackSlider");;
+	divDecaySlider = document.getElementById("decaySlider");;
+	divSustainSlider = document.getElementById("sustainSlider");;
+	divReleaseSlider = document.getElementById("releaseSlider");;
 	
 	window.onload = function() {
 	  context;
@@ -51,7 +72,7 @@ function setup(){
 	});
 
 	waveform = createSelect();
-	waveform.position(120,650);
+	//waveform.position(120,650);
 	var options = ['square','sine','triangle','sawtooth'];
     for (var i = 0; i < options.length; i++) {
     var option = createElement('option');
@@ -59,57 +80,65 @@ function setup(){
     option.html(options[i]);
     option.parent(waveform);
 	}
-	waveform.parent("mySidenav");
+	waveform.parent(divSelectWaveform);
 
     checkbox = createInput(0,1,0);               
     checkbox.attribute("type","checkbox");     
-    checkbox.position(115,700);
+    //checkbox.position(115,700);
     checkbox.attribute('checked', null);  
-    checkbox.parent("mySidenav");
+    checkbox.parent(divCheckbox);
  
 
     textAlign(CENTER);
     fill(45,145,237,255);
 
 	sliderDiameter = createSlider(5, 100, 25);
-	sliderDiameter.position(115, 80);
+	//sliderDiameter.position(115, 80);
 	sliderDiameter.style('width', '80px');
-	sliderDiameter.parent("mySidenav");
+	sliderDiameter.style('height', '7px');
+	sliderDiameter.parent(divDiameterSlider);
 
 	sliderFeedback = createSlider(0, 85, 30);
-	sliderFeedback.position(115, 150);
+	//sliderFeedback.position(115, 150);
 	sliderFeedback.style('width', '80px');
-	sliderFeedback.parent("mySidenav");
+	sliderFeedback.style('height', '7px');
+	sliderFeedback.parent(divFeedBackSlider);
 
 	sliderTime = createSlider(0, 90, 80);
-	sliderTime.position(115, 220);
+	//sliderTime.position(115, 220);
 	sliderTime.style('width', '80px');
-	sliderTime.parent("mySidenav");
+	sliderTime.style('height', '7px');	
+	sliderTime.parent(divTimeSlider);
 
 	sliderReverb = createSlider(0, 100, 50);
-	sliderReverb.position(115, 290);
+	//sliderReverb.position(115, 290);
 	sliderReverb.style('width', '80px');
-	sliderReverb.parent("mySidenav");
+	sliderReverb.style('height', '7px');
+	sliderReverb.parent(divReverbSlider);
 
-	sliderAttack = createSlider(0, 1, 0, 0);
-	sliderAttack.position(115, 360);
+	sliderAttack = createSlider(0, 3, 0, 0);
+	//sliderAttack.position(115, 360);
 	sliderAttack.style('width', '80px');
-	sliderAttack.parent("mySidenav");
+	sliderAttack.style('height', '7px');
+	sliderAttack.parent(divAttackSlider);
 
-	sliderDecay = createSlider(0, 1, 0.5, 0);
-	sliderDecay.position(115, 440);
+	sliderDecay = createSlider(0, 0.5, 0.5, 0);
+	//sliderDecay.position(115, 440);
 	sliderDecay.style('width', '80px');
-	sliderDecay.parent("mySidenav");
+	sliderDecay.style('height', '7px');
+	sliderDecay.parent(divDecaySlider);
 
 	sliderSustain = createSlider(0, 1, 0.5, 0);
-	sliderSustain.position(115, 510);
+	//sliderSustain.position(115, 510);
 	sliderSustain.style('width', '80px');
-	sliderSustain.parent("mySidenav");
+	sliderSustain.style('height', '7px');
+	sliderSustain.parent(divSustainSlider);
 
-	sliderRelease = createSlider(0, 2, 0.5, 0);
-	sliderRelease.position(115, 580);
+	sliderRelease = createSlider(0, 5, 0.5, 0);
+	//sliderRelease.position(115, 580);
 	sliderRelease.style('width', '80px');
-	sliderRelease.parent("mySidenav");
+	sliderRelease.style('height', '7px');
+	sliderRelease.parent(divReleaseSlider);
 
     env = new p5.Env();
 	env.setADSR(0.05, 0.1, 0.05, 0.05);
@@ -130,65 +159,68 @@ function setup(){
 
 }
 
+var estoyFueraDelBoton = true;
+boton = document.getElementById("boton");
+boton.addEventListener("mouseover", RespondMouseOver); 
+boton.addEventListener("mouseout", RespondMouseOut); 
+
+function RespondMouseOver() {
+	estoyFueraDelBoton = false;
+} 
+
+function RespondMouseOut() { 
+	estoyFueraDelBoton = true;
+} 
+
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("mySidenav").style.width = "250px";
+	menu = 250; 
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("mySidenav").style.width = "0";
+	menu = 0;
 }
 
 function centerCanvas() {
-  xcanvas = (windowWidth - width) / 2;
-  ycanvas = (windowHeight - height) / 2;
-  canvas.position(xcanvas, ycanvas);
-}
-
-function rehabAudio(canvas){
-canvas.addEventListener('click', function() {
-  audiocontext.resume().then(() => {
-    //console.log('Playback resumed successfully');
-  });
-});
+	xcanvas = (windowWidth - width) / 2;
+	ycanvas = (windowHeight - height) / 2;
+	canvas.position(xcanvas, ycanvas);
 }
 
 function windowResized() {
-  centerCanvas();
+	centerCanvas();
 }
 
 function mousePressed() {
-	if(mouseX > menu && mouseX >= boton + 15){
-	if (mouseX <= canvas.width || mouseX >= 0 && mouseY <= canvas.height || mouseY >= 0) {
-    nuevaPelotaX = mouseX;
-    nuevaPelotaY = mouseY;
-    var coords = "X coords: " + nuevaPelotaX + ", Y coords: " + nuevaPelotaY;
-    //print(coords);
-    var coordsdos = "equis" + mouseX + ",ye" + mouseY;
-	//print(coordsdos)
-	}
+	if(mouseX > menu && estoyFueraDelBoton && mouseX <= canvas.width && mouseY <= canvas.height){
+		nuevaPelotaX = mouseX;
+		nuevaPelotaY = mouseY;
 	}
 }
 
 
 function creaPelota() {
-	if(mouseX > menu && mouseX >= boton + 15){
-if (nuevaPelotaX != mouseX || nuevaPelotaY != mouseY){
-	circulos.push({
-			"x":mouseX,
-			"y":mouseY,
-			"d":(sliderDiameter.value()),
-			"speedX":(nuevaPelotaX - mouseX) / 30,
-			"speedY":(nuevaPelotaY - mouseY) / 30,
-			"color":[0, 0, 0]
-		});
-}
-}
+	if(mouseX > menu && estoyFueraDelBoton){
+		if (nuevaPelotaX != mouseX || nuevaPelotaY != mouseY){
+			circulos.push({
+				"x":mouseX,
+				"y":mouseY,
+				"d":(sliderDiameter.value()),
+				"speedX":(nuevaPelotaX - mouseX) / 30,
+				"speedY":(nuevaPelotaY - mouseY) / 30,
+				"color":[0, 0, 0]
+			});
+		}
+	}
 }
 
 function mouseReleased(){
-	if(mouseX > menu && mouseX >= boton + 15){
-	if (mouseX <= canvas.width || mouseX >= 0 && mouseY <= canvas.height || mouseY >= 0){
-		creaPelota();	}
+	if(mouseX > menu && estoyFueraDelBoton){
+		if (mouseX <= canvas.width || mouseX >= 0 && mouseY <= canvas.height || mouseY >= 0){
+			creaPelota();	
+		}
 	}
 }
 
@@ -201,68 +233,59 @@ function draw(){
 	var frecuencia = frecuencias[Math.floor(Math.random()*frecuencias.length)];
 	osc.setType(waveform.value());
 	delay.process(osc, sliderTime.value()/100, sliderFeedback.value()/100, 20000);
-	menu = document.getElementById("mySidenav").clientWidth;
-	boton = document.getElementById("boton").offsetWidth;
 
-
-
-	if (circulos.length==0) {
+	if (circulos.length < 1) {
     fill(0);
     textAlign(CENTER);
-    textSize(28);
-    text("mantene presionado el click y arrastra el mouse para empezar", canvas.width/2, canvas.height/2);
+	textSize(28);
+	textFont("Helvetica, Arial, sans-serif");
+    text("hold the left mouse button and drag the mouse to start", canvas.width/2, canvas.height/2);
     }
 
-	 	if (mouseIsPressed){
-	 		if(mouseX > menu && mouseX > boton + 15){
-	 			if(nuevaPelotaX <= canvas.width || nuevaPelotaX >= 0 && nuevaPelotaY <= canvas.height || nuevaPelotaY >= 0){
-     			ellipse(nuevaPelotaX, nuevaPelotaY, 8, 8);
-    			ellipse(mouseX, mouseY, diameter, diameter);
-    			line(nuevaPelotaX, nuevaPelotaY, mouseX, mouseY);
-		}
+	if (mouseIsPressed){
+		if(mouseX > menu && estoyFueraDelBoton){
+			if(nuevaPelotaX <= canvas.width || nuevaPelotaX >= 0 && nuevaPelotaY <= canvas.height || nuevaPelotaY >= 0){
+			ellipse(nuevaPelotaX, nuevaPelotaY, 8, 8);
+			ellipse(mouseX, mouseY, diameter, diameter);
+			line(nuevaPelotaX, nuevaPelotaY, mouseX, mouseY);
+			}	
 		}
 	}
-
 		
-
 	for (var i = 0; i < circulos.length; i++) {
-
         for (var j = i+1; j < circulos.length; j++){
-        hit = collideCircleCircle(circulos[i].x, circulos[i].y, circulos[i].d, circulos[j].x, circulos[j].y, circulos[j].d);
-
-         if (hit == true) {
-         env.play();
-        //console.log(circulos[i].y, circulos[j].y);
-         if (checkbox.elt.checked) {                           
-         osc.freq(frecuencia);
-         checkbox.value("on");
-        } else {
-         osc.freq(circulos[i].y + 60	);
-		 checkbox.value("off");       
-        }
-        hit = false;
-        background(random(0, 255), random(0, 255),random(0, 255));
-
+			hit = collideCircleCircle(circulos[i].x, circulos[i].y, circulos[i].d, circulos[j].x, circulos[j].y, circulos[j].d);
+			if (hit == true) {
+				env.play();
+				//console.log(circulos[i].y, circulos[j].y);
+				if (checkbox.elt.checked) {                           
+					osc.freq(frecuencia);
+					checkbox.value("on");
+				} else {
+					osc.freq(circulos[i].y + 60);
+					checkbox.value("off");       
+				}
+				hit = false;
+				background(random(0, 255), random(0, 255),random(0, 255));
+			}
     	}
-    }
-
-        
-
-	fill(circulos[i].color[0], circulos[i].color[1], circulos[i].color[2])
-	ellipse(circulos[i].x, circulos[i].y, circulos[i].d, circulos[i].d)
-	circulos[i].x += circulos[i].speedX;
-	circulos[i].y += circulos[i].speedY;
 
 
-	//para que las pelotas reboten
-	 if (circulos[i].x >= canvas.width || circulos[i].x <= 0)  {
-     	circulos[i].speedX = -circulos[i].speedX;
-  	}
+		fill(circulos[i].color[0], circulos[i].color[1], circulos[i].color[2])
+		ellipse(circulos[i].x, circulos[i].y, circulos[i].d, circulos[i].d)
+		circulos[i].x += circulos[i].speedX;
+		circulos[i].y += circulos[i].speedY;
 
 
-  	 if (circulos[i].y >= canvas.height || circulos[i].y <= 0)  {
-     	circulos[i].speedY = -circulos[i].speedY;
-  	}
+		//para que las pelotas reboten
+		if (circulos[i].x >= canvas.width || circulos[i].x <= 0)  {
+			circulos[i].speedX = -circulos[i].speedX;
+		}
+
+
+		if (circulos[i].y >= canvas.height || circulos[i].y <= 0)  {
+			circulos[i].speedY = -circulos[i].speedY;
+		}
 
     }
 
